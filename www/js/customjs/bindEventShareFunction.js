1b5a68aea586ad1cfbdf33b2d802231a8c420cd8
vcshing <vcshing2005@hotmail.com>
2017-05-09 23:14:20 +0800

    asd

--- www/js/customjs/bindEventShareFunction.js
+++ www/js/customjs/bindEventShareFunction.js
@@ -21,120 +21,134 @@ $(".badlist").bind("click", function(e) {
     $(".goodlist").removeClass("active");
     $(".badlist").addClass("active");
     getHomePageListRank("bad");
-//alert(2);
-	//	mainView.router.loadPage('indexRank.html')
+    //alert(2);
+    //	mainView.router.loadPage('indexRank.html')
 })
 
-$('.loginBtn').on('click', function () {
-//  myApp.loginScreen();
-  mainView.router.loadPage("login.html");
+$('.loginBtn').on('click', function() {
+    //  myApp.loginScreen();
+    mainView.router.loadPage("login.html");
 });
 
-$('.register').on('click', function () {
-  mainView.router.loadPage("register.html");
+$('.register').on('click', function() {
+    mainView.router.loadPage("register.html");
 });
 
 
-$('.startTutorials').on('click', function (e) {
+$('.startTutorials').on('click', function(e) {
 
 });
 
-$(".shareApp").bind("click",function(e){
-  window.plugins.socialsharing.share("Quote Designer", "Good Apps", "", "https://play.google.com/store/apps/details?id=com.skyexplorer.quotedesign");
+$(".shareApp").bind("click", function(e) {
+    window.plugins.socialsharing.share("Quote Designer", "Good Apps", "", "https://play.google.com/store/apps/details?id=com.skyexplorer.quotedesign");
 })
 
 $(".langen").bind("click", function() {
-  lang = "en";
-  storageManager.setCookie("lang",{"selectedLang": lang});
-  pageInit();
+    lang = "en";
+    storageManager.setCookie("lang", {
+        "selectedLang": lang
+    });
+    pageInit();
 
 })
 
 $(".langtc").bind("click", function() {
-//  alert(1);
-  lang = "zh-tw";
-  storageManager.setCookie("lang",{"selectedLang": lang});
-  pageInit();
+    //  alert(1);
+    lang = "zh-tw";
+    storageManager.setCookie("lang", {
+        "selectedLang": lang
+    });
+    pageInit();
 })
 
 $(".langDefault").bind("click", function() {
-  lang = checkLang();
-  storageManager.setCookie("lang",{"selectedLang": lang});
-  pageInit();
+    lang = checkLang();
+    storageManager.setCookie("lang", {
+        "selectedLang": lang
+    });
+    pageInit();
 })
 
 
-$$('.designFormSubmit').on('click', function(){
+$$('.designFormSubmit').on('click', function() {
 
-var formData = myApp.formToJSON('#designFormSubmit');
-formData['locale'] = lang;
-if(typeof(device)=="undefined"){
-	formData['deviceid'] = "";
-}else{
-	formData['deviceid'] = device.uuid;
-}
+    var formData = myApp.formToJSON('#designFormSubmit');
+    formData['locale'] = lang;
+    if (typeof(device) == "undefined") {
+        formData['deviceid'] = "";
+    } else {
+        formData['deviceid'] = device.uuid;
+    }
 
-formData['userid'] = '';
+    formData['userid'] = '';
 
- $.ajax({
-  type: 'POST',
-  url: 'http://gogogo.synology.me/api/genword/genImg.php',
-  data: formData,
-  dataType: 'JSON',
-  success: function(response){
-	  if(response.status==1){
-		  $(".designImage").attr("src",response.domainPath)
-		  $(".designImage").attr("data-id",response.insertId)
-	  }else{
-		  alert("Server Error, Please Try Again Later1");
-	  }
-  },
-	error: function(response) {
-		alert("Please Enter Value");
-	}
-});
+    $.ajax({
+        type: 'POST',
+        url: 'http://gogogo.synology.me/api/genword/genImg.php',
+        data: formData,
+        dataType: 'JSON',
+        success: function(response) {
+            if (response.status == 1) {
+                $(".designImage").attr("src", response.domainPath)
+                $(".designImage").attr("data-id", response.insertId)
+            } else {
+                alert("Server Error, Please Try Again Later1");
+            }
+        },
+        error: function(response) {
+            alert("Please Enter Value");
+        }
+    });
 })
 
 
-$(".designFormPublish").bind("click",function(){
-	if($(".designImage").attr("data-id") != undefined){
-		 $.ajax({
-		  type: 'POST',
-		  url: 'http://gogogo.synology.me/api/genword/publish.php',
-		  data: {"masterid":$(".designImage").attr("data-id")},
-		  dataType: 'JSON',
-		  success: function(response){
-			  if(response.status==1){
-				myApp.alert("Publish Success");
-				}
-			}
-		 })
-	}
+$(".designFormPublish").bind("click", function() {
+    if ($(".designImage").attr("data-id") != undefined) {
+        $.ajax({
+            type: 'POST',
+            url: 'http://gogogo.synology.me/api/genword/publish.php',
+            data: {
+                "masterid": $(".designImage").attr("data-id")
+            },
+            dataType: 'JSON',
+            success: function(response) {
+                if (response.status == 1) {
+                    myApp.alert("Publish Success");
+                }
+            }
+        })
+    }
 })
 
 var enableDownload = 1;
-$(".designFormDownload").bind("click",function(){
-	
-	if($(".designImage").attr("data-id") != undefined){
-		
-		if(enableDownload == 1){
-			enableDownload =0;
-			
-			imgurl=$(".designImage").attr("src");
-			//DownloadFile(imgurl, "Cosplay", MD5(imgurl)) ;
-			
-			var success = function(msg){
-				myApp.alert("Saved to gallery");
-				enableDownload =1;
-			};
+$(".designFormDownload").bind("click", function() {
 
-			var error = function(err){
-				alert("Fail to Save");
-				enableDownload =1;
-			};
+    if ($(".designImage").attr("data-id") != undefined) {
 
-			saveImageToPhone(encodeURI(imgurl), success, error);
+        if (enableDownload == 1) {
+            enableDownload = 0;
 
-		}
-	}
+            imgurl = $(".designImage").attr("src");
+            //DownloadFile(imgurl, "Cosplay", MD5(imgurl)) ;
+
+            var success = function(msg) {
+                myApp.alert("Saved to gallery");
+                enableDownload = 1;
+            };
+
+            var error = function(err) {
+                alert("Fail to Save");
+                enableDownload = 1;
+            };
+
+            saveImageToPhone(encodeURI(imgurl), success, error);
+
+        }
+    }
 })
+
+$(".designFormShare").bind("click", function(e) {
+    if ($(".designImage").attr("data-id") != undefined) {
+        window.plugins.socialsharing.share(" ", " ", $(".designImage").attr("src") , "https://play.google.com/store/apps/details?id=com.skyexplorer.quotedesign");
+    }
+});
--- www/js/customjs/initScript.js
+++ www/js/customjs/initScript.js
@@ -21,82 +21,124 @@ myApp.onPageInit('login', function(page) {
 })
 
 myApp.onPageInit('quotelist', function(page) {
- var cardHtml="";
-// Loading flag
-var loading = false;
- 
-var page=1;
- // Attach 'infinite' event handler
-appendDesignCard();
+    var cardHtml = "";
+    // Loading flag
+    var loading = false;
 
-$$('.infinite-scroll').on('infinite', function () {
-  // Exit, if loading in progress
-  if (loading) return;
-  // Set loading flag
-  loading = true;
-  
-  setTimeout(function () {appendDesignCard()  }, 1000);
-});        
+    var page = 1;
+    // Attach 'infinite' event handler
+    appendDesignCard();
 
-function appendDesignCard(){
+    $$('.infinite-scroll').on('infinite', function() {
+        // Exit, if loading in progress
+        if (loading) return;
+        // Set loading flag
+        loading = true;
 
-    // Reset loading flag
-    loading = false;
- 
- //   if (lastIndex >= maxItems) {
-      // Nothing more to load, detach infinite scroll events to prevent unnecessary loadings
- //     myApp.detachInfiniteScroll($$('.infinite-scroll'));
-      // Remove preloader
- //     $$('.infinite-scroll-preloader').remove();
- //     return;
-  //  }
- 
-
- $.ajax({
-  type: 'POST',
-  url: 'http://gogogo.synology.me/api/genword/getlist.php',
-  data: {"page":page,"lang":lang},
-  dataType: 'JSON',
-  success: function(response){
-	  if(response.status==1){
-
-		if(response.result.length==0){
-		  myApp.detachInfiniteScroll($$('.infinite-scroll'));
-		  $$('.infinite-scroll-preloader').remove();
-		  return;
-		}else{
-		
-		 for (var i = 0; i < response.result.length ; i++) {
-			cardHtml += "			<div class=\"card\">";
-			cardHtml += "				  <div class=\"card-content\">";
-			cardHtml += "";
-			cardHtml += "<img class=\"designImage\" data-id=\""+response.result[i].master_id+"\" src=\""+response.result[i].path+"\" width=\"100%\">";
-			cardHtml += "";
-			cardHtml += "				  <\/div>";
-			cardHtml += "				  <div class=\"card-footer\">";
-			cardHtml += "					<a href=\"#\" class=\"link\"  data-id=\""+response.result[i].master_id+"\" >Like ("+response.result[i].likecount+")<\/a>";
-			cardHtml += "				  <\/div>";
-			cardHtml += "				<\/div>";
-		 }
-		}
-		
-		 
-			// Append new items
-			$$('.loopCardContent').append(cardHtml);
-		 
-			// Update last loaded index
-			page++;
-	  }else{
-		 // alert("Server Error, Please Try Again Later");
-	  }
-  },
-	error: function(response) {
-		//alert("Server Error, Please Try Again Later");
-	}
-});
+        setTimeout(function() {
+            appendDesignCard()
+        }, 1000);
+    });
 
 
 
-	
-} 
+
+    function appendDesignCard() {
+        loading = false;
+        $.ajax({
+            type: 'POST',
+            url: 'http://gogogo.synology.me/api/genword/getlist.php',
+            data: {
+                "page": page,
+                "lang": lang
+            },
+            dataType: 'JSON',
+            success: function(response) {
+                if (response.status == 1) {
+
+                    if (response.result.length == 0) {
+                        myApp.detachInfiniteScroll($$('.infinite-scroll'));
+                        $$('.infinite-scroll-preloader').remove();
+                        return;
+                    } else {
+
+                        for (var i = 0; i < response.result.length; i++) {
+                            cardHtml += "			<div class=\"card\">";
+                            cardHtml += "				  <div class=\"card-content\">";
+                            cardHtml += "";
+                            cardHtml += "<img class=\"designImage\" data-id=\"" + response.result[i].master_id + "\" src=\"" + response.result[i].path + "\" width=\"100%\">";
+                            cardHtml += "";
+                            cardHtml += "				  <\/div>";
+                            cardHtml += "				  <div class=\"card-footer\">";
+                            cardHtml += "					<a href=\"#\" class=\"publishQuoteListLike\"  data-id=\"" + response.result[i].master_id + "\" >Like (" + response.result[i].likecount + ")<\/a>";
+                            cardHtml += "		<a href=\"#\" class=\"publishQuoteListShare\"  data-path=\"" + response.result[i].path + "\" >Share<\/a>";
+                            cardHtml += "		<a href=\"#\" class=\"publishQuoteListDownload\"  data-path=\"" + response.result[i].path + "\" >Download<\/a>";
+                            cardHtml += "				  <\/div>";
+                            cardHtml += "				<\/div>";
+                        }
+                    }
+
+
+                    // Append new items
+                    $$('.loopCardContent').append(cardHtml);
+
+                    // Update last loaded index
+                    page++;
+
+                    $(".publishQuoteListLike").bind("click", function(e) {
+                        if (getCookie("like", $(this).attr("data-id"), "0") != "1") {
+                            $.ajax({
+                                type: 'POST',
+                                url: 'http://gogogo.synology.me/api/genword/addlike.php',
+                                data: {
+                                    "masterid": $(this).attr("data-id"),
+                                },
+                                dataType: 'JSON',
+                                success: function(response) {
+                                    if (response.status == 1) {
+                                        setCookieIndex("like", $(this).attr("data-id"), "1");
+                                    }
+                                }
+                            });
+                        }
+                    });
+
+                    $(".publishQuoteListShare").bind("click", function(e) {
+                        window.plugins.socialsharing.share(" ", " ", $(this).attr("data-path"), "https://play.google.com/store/apps/details?id=com.skyexplorer.quotedesign");
+                    });
+
+                    var enableDownload = 1;
+                    $(".publishQuoteListDownload").bind("click", function(e) {
+                        if (enableDownload == 1) {
+                            enableDownload = 0;
+debugger;
+                            imgurl = $(this).attr("data-path");
+                            //DownloadFile(imgurl, "Cosplay", MD5(imgurl)) ;
+
+                            var success = function(msg) {
+                                myApp.alert("Saved to gallery");
+                                enableDownload = 1;
+                            };
+
+                            var error = function(err) {
+                                alert("Fail to Save");
+                                enableDownload = 1;
+                            };
+
+                            saveImageToPhone(encodeURI(imgurl), success, error);
+
+                        }
+
+                    });
+
+
+                } else {
+                    // alert("Server Error, Please Try Again Later");
+                }
+            },
+            error: function(response) {
+                //alert("Server Error, Please Try Again Later");
+            }
+        });
+    }
 })
